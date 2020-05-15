a5edf765a3150160839270dcbd21190ad861b07c
Philipp <philipp.panhey@bo-bremen.de>
2020-05-15 12:32:32 +0200

    implemented null cleaning

--- pandas_stat.py
+++ pandas_stat.py
@@ -2,6 +2,7 @@ import requests
 import bs4
 from bs4 import BeautifulSoup
 import json
+import statistics
 
 
 def extractReproductionNumbers(state):
@@ -16,18 +17,36 @@ def extractReproductionNumbers(state):
     reproduction_numbers = data["x"]["data"][0]["y"]
     date_of_reproduction_numbers = data["x"]["data"][0]["x"]
 
+    reproduction_numbers = getRidOfNone(reproduction_numbers)
+    date_of_reproduction_numbers = getRidOfNone(date_of_reproduction_numbers)
+
     return {
         "dates": date_of_reproduction_numbers,
         "numbers": reproduction_numbers,
     }
 
 
+def getRidOfNone(array):
+    return [x for x in array if x is not None]
+
+
 def splitSeperationNumbersByDate(reproduction_numbers_dataset, date_str):
-    entry_index = reproduction_numbers_dataset['dates'].index(date_str)
-    no_lockdown = reproduction_numbers_dataset['numbers'][:entry_index]
-    lockdown = [x for x in reproduction_numbers_dataset['numbers'] if x not in no_lockdown]
+    entry_index = reproduction_numbers_dataset["dates"].index(date_str)
+    no_lockdown = reproduction_numbers_dataset["numbers"][:entry_index]
+    lockdown = [
+        x for x in reproduction_numbers_dataset["numbers"] if x not in no_lockdown
+    ]
     return no_lockdown, lockdown
 
 
-reproduction_numbers_dataset = extractReproductionNumbers("bremen")
-splitSeperationNumbersByDate(reproduction_numbers_dataset, "2020-03-12")
+state = "bremen"
+lockdown_date = "2020-03-24"
+reproduction_numbers_dataset = extractReproductionNumbers(state)
+no_lockdown, lockdown = splitSeperationNumbersByDate(
+    reproduction_numbers_dataset, lockdown_date
+)
+
+
+print("mittelwert r no_lockdown in " + state + ": " + str(statistics.mean(no_lockdown)))
+print('####')
+print("mittelwert r lockdown in " + state + ": " + str(statistics.mean(lockdown)))
